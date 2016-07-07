697cc2194b3bd318d2087d766fe4c05fab4c2fbb
fox <fox@moneyball.io>
2016-07-07 23:51:11 +0800

    Rendering and working with lists (#7)
    * Add training 7 html file
    
    * Add tradining 7 readme url link
    
    * Add main js file
    
    * Add tasks list
    
    * Add show tasks list
    
    * Add tasks list data
    
    * Add class binding data
    
    * Add task list completed class
    
    * Add task completed toggle class
    
    * Add task completed use inline

---
+++ main.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    tasks: [
+      { body: 'Go to the store', completed: false },
+      { body: 'Go to the bank', completed: false },
+      { body: 'Go to the doctor', completed: true }
+    ]
+  }
+});
